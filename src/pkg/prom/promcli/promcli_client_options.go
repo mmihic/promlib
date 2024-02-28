092a96870c6d8d862aa570a0e02b4975da7a13a6
Matt Mihic <matt.mihic@gmail.com>
2024-02-28 10:26:33 -0800

    Default to Chronosphere URL and token.

--- src/pkg/prom/promcli/promcli_client_options.go
+++ src/pkg/prom/promcli/promcli_client_options.go
@@ -12,32 +12,28 @@ import (
 	"promlib/src/pkg/prom"
 )
 
+const (
+	chronoPrometheusURL = "https://%s.chronosphere.io/data/m3/"
+)
+
 // ClientOptions are options for creating a client on the command line.
 type ClientOptions struct {
 	APITokenFile string `help:"file containing the API token"`
-	ServerURL    string `name:"server-url" short:"s" required:"" help:"Prometheus server URL"`
+	ServerURL    string `name:"server-url" short:"s" help:"Prometheus server URL"`
+	Tenant       string `name:"tenant" short:"t" help:"name of the Chronosphere tenant to query"`
 	LogQueries   bool   `help:"set to log queries"`
 }
 
 // PromClient returns a prom PromClient.
 func (opts *ClientOptions) PromClient(_ context.Context) (prom.Client, error) {
-	var apiToken string
-
-	if len(opts.APITokenFile) != 0 {
-		apiTokenBytes, err := os.ReadFile(opts.APITokenFile)
-		if err != nil {
-			return nil, err
-		}
-
-		apiToken = string(apiTokenBytes)
+	apiToken, client, err := opts.getAPIToken()
+	if err != nil {
+		return client, err
 	}
 
-	if len(apiToken) == 0 {
-		apiToken = os.Getenv("PROM_API_TOKEN")
-	}
-
-	if len(apiToken) == 0 {
-		return nil, errors.New("neither --api-token-file nor PROM_API_TOKEN env var are set")
+	baseURL, err := opts.getBaseURL()
+	if err != nil {
+		return nil, err
 	}
 
 	clientOpts := []prom.ClientOpt{
@@ -52,5 +48,44 @@ func (opts *ClientOptions) PromClient(_ context.Context) (prom.Client, error) {
 		clientOpts = append(clientOpts, prom.WithQueryLog(log))
 	}
 
-	return prom.NewClient(opts.ServerURL, clientOpts...)
+	return prom.NewClient(baseURL, clientOpts...)
+}
+
+func (opts *ClientOptions) getBaseURL() (string, error) {
+	var baseURL string
+	switch {
+	case len(opts.ServerURL) != 0 && len(opts.Tenant) != 0:
+		return "", errors.New("only one of --tenant or --server-url must be specified")
+	case len(opts.ServerURL) != 0:
+		baseURL = opts.ServerURL
+	case len(opts.Tenant) != 0:
+		baseURL = fmt.Sprintf(chronoPrometheusURL, opts.Tenant)
+	}
+
+	if len(baseURL) == 0 {
+		return "", errors.New("one of --tenant or --server-url must be specified")
+	}
+	return baseURL, nil
+}
+
+func (opts *ClientOptions) getAPIToken() (string, prom.Client, error) {
+	var apiToken string
+
+	if len(opts.APITokenFile) != 0 {
+		apiTokenBytes, err := os.ReadFile(opts.APITokenFile)
+		if err != nil {
+			return "", nil, err
+		}
+
+		apiToken = string(apiTokenBytes)
+	}
+
+	if len(apiToken) == 0 {
+		apiToken = os.Getenv("PROM_API_TOKEN")
+	}
+
+	if len(apiToken) == 0 {
+		return "", nil, errors.New("neither --api-token-file nor PROM_API_TOKEN env var are set")
+	}
+	return apiToken, nil, nil
 }
